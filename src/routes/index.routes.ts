587671d797d978b8cbf2fc529476fbd488037963
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-09-24 17:34:06 +0530

    routes updates

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,9 +1,13 @@
-import { Express } from "express";
+import { Express, Request, Response } from "express";
 import postRoutes from "./post.routes";
 import commentRoutes from "./comment.routes";
 import metricsRoutes from "./metrics.routes";
 
 const loadRoutes = (app: Express) => {
+  app.get("/", async (_: Request, res: Response) =>
+    res.send("Server is up and running...")
+  );
+
   app.use("/api/v1/post", postRoutes);
   app.use("/api/v1/comment", commentRoutes);
   app.use("/prometheus", metricsRoutes);
