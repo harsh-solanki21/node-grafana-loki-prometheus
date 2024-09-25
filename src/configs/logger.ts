8f11535c9bc0697d588cf910dbcbbaf611873999
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-09-25 16:13:26 +0530

    prom updates

--- src/configs/logger.ts
+++ src/configs/logger.ts
@@ -1,25 +1,21 @@
-import winston from "winston";
+import { createLogger, transports, format } from "winston";
 import LokiTransport from "winston-loki";
 
 const logConfig = {
-  level: process.env.LOG_LEVEL || "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
+  format: format.combine(format.timestamp(), format.json()),
   transports: [
-    new winston.transports.Console(),
+    new transports.Console(),
     new LokiTransport({
-      host: "http://localhost:3100",
-      labels: { job: "node-app" },
+      host: "http://192.168.29.238:3100", // Your IP address
+      labels: { app: "node-app" },
       json: true,
-      format: winston.format.json(),
+      format: format.json(),
       replaceTimestamp: true,
       onConnectionError: (err) => console.error(err),
     }),
   ],
 };
 
-const logger = winston.createLogger(logConfig);
+const logger = createLogger(logConfig);
 
 export default logger;
